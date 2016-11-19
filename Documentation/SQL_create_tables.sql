8a60b6281f76d32cd068b19f173290025e5ba25a
fpsoares <fprofeta81@gmail.com>
2016-11-18 22:50:22 -0500

    Updated Create SQL

--- Documentation/SQL_create_tables.sql
+++ Documentation/SQL_create_tables.sql
@@ -23,4 +23,3 @@ CREATE table ref_job_position (id INT NOT NULL AUTO_INCREMENT, position_name var
 CREATE table ref_degree (id INT NOT NULL AUTO_INCREMENT, degree_name varchar(30), PRIMARY KEY (id));
 
 
-
