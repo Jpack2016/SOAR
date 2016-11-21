e4f2da1fda5b3160b85cceee406976695d973f02
fpsoares <fprofeta81@gmail.com>
2016-11-20 19:49:46 -0500

    Poster PPT and Resources

---
+++ Documentation/proficiency.sql
@@ -0,0 +1,4 @@
+insert into ref_proficiency (proficiency_name) values ('Novice');
+insert into ref_proficiency (proficiency_name) values ('Intermediate');
+insert into ref_proficiency (proficiency_name) values ('Advanced');
+insert into ref_proficiency (proficiency_name) values ('Expert');
