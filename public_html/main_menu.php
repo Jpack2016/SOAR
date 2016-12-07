0ad6094f06a1fdcd1f79eec7306a435ec671381e
fpsoares <fprofeta81@gmail.com>
2016-12-07 18:08:09 -0500

    Unhide session vars

--- public_html/main_menu.php
+++ public_html/main_menu.php
@@ -43,7 +43,7 @@ right: 0;
 .stylez{
         list-style: none;
         text-align: center;
-    
+
     }
 li{
         display: inline-block;
@@ -60,22 +60,22 @@ li{
     <li><button class="button"><span>Search Database </span></button></li>
 </ul>
 
-<!--
+
 <div class="wrapper">
     <h1>Google Profile Details </h1>
     <?php
-    // echo '<div class="welcome_txt">Welcome <b>'.$_SESSION['google_data']['given_name'].'</b></div>';
-    // echo '<div class="google_box">';
-    // echo '<p class="image"><img src="'.$_SESSION['google_data']['picture'].'" alt="" width="300" height="220"/></p>';
-    // echo '<p><b>Google ID : </b>' . $_SESSION['google_data']['id'].'</p>';
-    // echo '<p><b>Name : </b>' . $_SESSION['google_data']['name'].'</p>';
-    // echo '<p><b>Email : </b>' . $_SESSION['google_data']['email'].'</p>';
-    // echo '<p><b>Gender : </b>' . $_SESSION['google_data']['gender'].'</p>';
-    // echo '<p><b>Locale : </b>' . $_SESSION['google_data']['locale'].'</p>';
-    // echo '<p><b>Google+ Link : </b>' . $_SESSION['google_data']['link'].'</p>';
-    // echo '<p><b>You are login with : </b>Google</p>';
-    // echo '<p><b>Logout from <a href="logout.php?logout">Google</a></b></p>';
-    // echo '</div>';
+    echo '<div class="welcome_txt">Welcome <b>'.$_SESSION['google_data']['given_name'].'</b></div>';
+     echo '<div class="google_box">';
+     echo '<p class="image"><img src="'.$_SESSION['google_data']['picture'].'" alt="" width="300" height="220"/></p>';
+     echo '<p><b>Google ID : </b>' . $_SESSION['google_data']['id'].'</p>';
+     echo '<p><b>Name : </b>' . $_SESSION['google_data']['name'].'</p>';
+     echo '<p><b>Email : </b>' . $_SESSION['google_data']['email'].'</p>';
+     echo '<p><b>Gender : </b>' . $_SESSION['google_data']['gender'].'</p>';
+     echo '<p><b>Locale : </b>' . $_SESSION['google_data']['locale'].'</p>';
+     echo '<p><b>Google+ Link : </b>' . $_SESSION['google_data']['link'].'</p>';
+     echo '<p><b>You are login with : </b>Google</p>';
+     echo '<p><b>Logout from <a href="logout.php?logout">Google</a></b></p>';
+     echo '</div>';
     ?>
-</div> -->
+</div>
 </body>
