025175c72213405fcfa7519a9fa97d487d15bd1e
fpsoares <fprofeta81@gmail.com>
2016-12-08 13:46:09 -0500

    Moved CSS to external file

--- public_html/index.php
+++ public_html/index.php
@@ -6,16 +6,8 @@
  <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="main.css">
 <style>
-body {
-    height:100%;
-    align-self: center;
-    background: #558BD1; /* For browsers that do not support gradients */
-    background: -webkit-linear-gradient(left top, #062C5E, #558BD1); /* For Safari 5.1 to 6.0 */
-    background: -o-linear-gradient(bottom right, #062C5E, #558BD1); /* For Opera 11.1 to 12.0 */
-    background: -moz-linear-gradient(bottom right, #062C5E, #558BD1); /* For Firefox 3.6 to 15 */
-    background: linear-gradient(to bottom right, #062C5E, #558BD1); /* Standard syntax (must be last) */
-}
 
 #loginbox {
     height:100%;
@@ -24,29 +16,6 @@ body {
     display:block;
     padding:50px;
     padding-bottom:350px;
-    
-
-}
-a {
-  color:white;
-  font-family: "Verdana", Verdana, sans-serif;
-  size:100%;
-
-}
-
-img {
-  display:block;
-  border: none;
-  border-radius: 4px;
-  padding: 5px;
-  margin:auto;
-  width:400px;
-}
-
-form {
-  margin:auto;
-  border-radius: 3px;
-
 }
 
 #welcome {
@@ -60,33 +29,14 @@ form {
   text-align:center;
 }
 
-input[type=button], input[type=submit], input[type=reset] {
-    background-color: #00426B; /* FAU Blue */
-    border: none;
-    border-radius:4px;
-    color: white;
-    padding: 16px 32px;
-    text-decoration: none;
-    margin: 4px 2px;
-    cursor: pointer;
-    text-align: center;
-    width:200px;
-}
-
-input[type=text], input[type=password] {
-    border:thin;
-    border-color:#B60215;
-    border-radius:4px;
-}
-
 </style>
 <body>
 <div id="loginbox">
 <img src="images/logo.png" alt="Logo"><br>
 <a id="welcome">Welcome to SOAR<br>
-A Resume Builder</a><br><br> 
+A Resume Builder</a><br><br>
 <center>
-  <form action='main_menu.php'>
+  <form action='userauth.php'>
       <a>Username:</a>&nbsp;
       <input type="text" name="userId"><br><br>
       <a>Password:&nbsp;&nbsp;</a>
