cf1e07542591a1b625ba841a85c1ec4a81c98cb3
fpsoares <fprofeta81@gmail.com>
2016-12-08 09:34:57 -0500

    Upate Login
    centered contents, added logo ref and CSS changes

--- public_html/index.php
+++ public_html/index.php
@@ -9,6 +9,7 @@
 <style>
 #grad1 {
     align-self: center;
+    height: 100%;
     background: red; /* For browsers that do not support gradients */
     background: -webkit-linear-gradient(left top, #062C5E, #558BD1); /* For Safari 5.1 to 6.0 */
     background: -o-linear-gradient(bottom right, #062C5E, #558BD1); /* For Opera 11.1 to 12.0 */
@@ -26,22 +27,44 @@ img {
   border: 1px solid #ddd;
   border-radius: 4px;
   padding: 5px;
+  margin:auto;
+  width:40%;
 }
 form {
   margin:auto;
   width:40%;
+
+}
+
+#welcome {
+  font-weight: bold;
+  font-size: 2.5em;
+  font-family: "Verdana", Verdana, sans-serif;
+  margin:auto;
+  width:40%;
+  color:#B60215
+}
+.button {
+    background-color: #00426B; /* FAU Blue */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
 }
 </style>
 <body >
 <div id="grad1">
 <div id="loginbox">
 
-<img src="logo.png" alt="Logo"><br>
-<a class="welcome">Welcome to SOAR</a>
+<img src="images/logo.png" alt="Logo"><br>
+<a id="welcome">Welcome to SOAR</a>
   <form >
-      User name:
+      <a color:white>User name:</a>
       <input type="text" name="userId"><br><br>
-      Password:
+      <a color:white>Password:&nbsp;&nbsp;</a>
       <input type="password" name="userPass"><br>
       <input type="submit" class='button' value="Login">&nbsp;&nbsp;
       <a href='resetPassword.php'>Forgot your password?</a>
