077d0956c54ef378f98d42f515cdd342a00b616d
fpsoares <fprofeta81@gmail.com>
2016-12-08 22:25:50 -0500

    Changed template array call

--- public_html/resumeDocOut.php
+++ public_html/resumeDocOut.php
@@ -72,7 +72,7 @@ if (isset($_POST['debug']) && ($_POST['debug']=='show'))    $TBS->Plugin(OPENTBS
 // --------------------------------------------
 
 // Merge data in the body of the document
-$TBS->MergeBlock('a,b', $data);
+$TBS->MergeBlock('a', $data);
 
 // Change chart series
 $ChartNameOrNum = 'a nice chart'; // Title of the shape that embeds the chart
