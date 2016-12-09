e7c4186971b28c896075540f9ef9214bdc59aace
fpsoares <fprofeta81@gmail.com>
2016-12-09 03:16:23 -0500

    Fixing reset pwd bkgn

--- public_html/reset_password.php
+++ public_html/reset_password.php
@@ -7,13 +7,13 @@ require_once('reset.php');
 <head>
 <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
-<body>
+<body background="background.png">
 <div id="resetbox">
 <center>
   <a>Enter your email to receive a new password</a><br>
   <form action='reset_password.php'>
       <a>Email:</a>
-      <input type="text" name="email">
+      <input type="text" name="email"><br>
       <input type="submit" value="Reset">
   </form>
 </center>
