4d39d5960e4f3c0673555f9877c7d8299d9571e9
fpsoares <fprofeta81@gmail.com>
2016-12-09 02:01:23 -0500

    fixed dbconnect

--- public_html/db_connect.php
+++ public_html/db_connect.php
@@ -5,8 +5,8 @@ $server_root = dirname($_SERVER['PHP_SELF']);
 // You will need to require this file on EVERY php file that uses the database.
 // Be sure to use $db->close(); at the end of each php file that includes this!
 $dbhost = 'localhost';  // Most likely will not need to be changed
-$dbname = 'fsoare2014';   // Needs to be changed to your designated table database name
-$dbuser = 'fsoare2014';   // Needs to be changed to reflect your LAMP server credentials
+$dbname = 'fsoares2014';   // Needs to be changed to your designated table database name
+$dbuser = 'fsoares2014';   // Needs to be changed to reflect your LAMP server credentials
 $dbpass = 'YD493oOEd3'; // Needs to be changed to reflect your LAMP server credentials
 $db = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 if($db->connect_errno > 0) {
