019496a150de1436091f8542a7d0f07f497d23c3
fsoares2014 09-2016 <fsoares2014@lamp.eng.fau.edu>
2016-12-09 04:27:09 -0500

    cameron

--- public_html/search.php
+++ public_html/search.php
@@ -1,61 +1,50 @@
 <?php
     require_once 'db_connect.php';
     echo "actually trying";
+    if($_POST['search']=='study')
+    {
+      $sql='SELECT study_area FROM ref_study_area ORDER BY study_area ASC';
+      $result = mysql_query($sql);
+      $dropdown=array();
 
-    $sql="SELECT skill_name FROM ref_skills ORDER BY skill_name ASC";
-    $result = mysql_query($sql);
-    echo $result;
-    echo $sql;
+      while($row = mysql_fetch_array($result)) {
+        array_push($dropdown, $row['degree_name']);
+      }
+    }
 
-  //   if($_POST['search']=='study')
-  //   {
-  //     $sql="SELECT study_area FROM ref_study_area ORDER BY study_area ASC";
-  //     $result = mysql_query($sql);
-  //     $dropdown=array();
-  //
-  //     while($row = mysql_fetch_array($result)) {
-  //       array_push($dropdown, $row['degree_name']);
-  //     }
-  //   }
-  //
-  //   if($_POST['search']=='degree')
-  //   {
-  //     $sql="SELECT degree_name FROM ref_degree  ORDER BY degree_name ASC";
-  //     $result = mysql_query($sql);
-  //     $dropdown=array();
-  //
-  //     while($row = mysql_fetch_array($result)) {
-  //       array_push($dropdown, $row['degree_name']);
-  //     }
-  //   }
-  //
-  //   if($_POST['search']=='job')
-  //   {
-  //     $sql="SELECT position_name FROM ref_job_position ORDER BY position_name ASC";
-  //     $result = mysql_query($sql);
-  //     $dropdown=array();
-  //
-  //     while($row = mysql_fetch_array($result)) {
-  //       array_push($dropdown, $row['position_name']);
-  //     }
-  //   }
-  //
-  //   if($_POST['search']=='skill')
-  //   {
-  //     $sql="SELECT skill_name FROM ref_skills ORDER BY skill_name ASC";
-  //     $result = mysql_query($sql);
-	// echo $result;
-	// echo $sql;
-  //     $dropdown=array();
-  //
-  //       while($row = mysql_fetch_array($result)) {
-  //         array_push($dropdown, $row['skill_name']);
-  //       }
-  //   }
-  //
-  //   print_r($dropdown);
+    if($_POST['search']=='degree')
+    {
+      $sql='SELECT degree_name FROM ref_degree  ORDER BY degree_name ASC';
+      $result = mysql_query($sql);
+      $dropdown=array();
+
+      while($row = mysql_fetch_array($result)) {
+        array_push($dropdown, $row['degree_name']);
+      }
+    }
+
+    if($_POST['search']=='job')
+    {
+      $sql='SELECT position_name FROM ref_job_position ORDER BY position_name ASC';
+      $result = mysql_query($sql);
+      $dropdown=array();
+
+      while($row = mysql_fetch_array($result)) {
+        print_r($row);
+      }
+    }
+
+    if($_POST['search']=='skill')
+    {
+      $query = $db->query("SELECT skill_name FROM ref_skills ORDER BY skill_name ASC");
+      $dropdown=array();
+        while($row = mysql_fetch_assoc($query)) {
+          array_push($dropdown, $row['skill_name']);
+        }
+    }
+
+    print_r($dropdown);
 
 
 
 ?>
->>>>>>> Stashed changes
