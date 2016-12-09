cd5a50f11cbc20cdd157854cac59b09275f03340
fsoares2014 09-2016 <fsoares2014@lamp.eng.fau.edu>
2016-12-09 08:53:52 -0500

    adding new files

---
+++ public_html/results.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <title>SOAR Search</title>
+
+    <link href="soarsearch.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" />
+    <style>
+    table {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        text-align: center;
+        width: 90%;
+    }
+
+    th {
+      background-color: #ffffff;
+      border: 1px solid #dddddd;
+      text-align: center;
+      color: blue;
+      padding: 8px;
+    }
+
+    td {
+        border: 1px solid #000;
+        color: red;
+        text-align: center;
+        padding: 8px;
+    }
+
+    tr:nth-child(even) {
+        background-color: #dddddd;
+    }
+
+    tr:nth-child(odd) {
+      background-color: #fff;
+    }
+    #center {
+      padding-top: 3%;
+      text-align: center;
+      padding-left: 5%;
+    }
+    </style>
+
+</head>
+
+<body background="background.png">
+
+  <div id="center">
+
+    <table>
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Z-Number</th>
+        <th>Email</th>
+        <th>Trait</th>
+        <th>Resume</th>
+      </tr>
+      <tr>
+        <td>Cameron</td>
+        <td>Herwig</td>
+        <td>23224265</td>
+        <td>cherwig2012@fau.edu</td>
+        <td>PHP-5</td>
+        <td><a href="23224265.odt">Results</a></td>
+
+      </tr>
+
+      <tr>
+        <td>Jacob</td>
+        <td>Pack</td>
+        <td>23238227</td>
+        <td>jpack2012@fau.edu</td>
+        <td>PHP-5</td>
+        <td><a href="23238227.odt">Results</a></td>
+
+      </tr>
+
+    </table>
+
+  </div>
+
+</body>
+</html>
